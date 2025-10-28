972b2c02263c068e694d17300fa2eedef4a11860
Roberto Delgado Sanchez <egl33817@educastur.es>
2025-10-28 17:06:16 +0100

    Se crea la lista de criptomonedas, se agregó Axios y se creó el archivo App.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react"
 import { useEffect } from "react"
+import axios from "axios"
 
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
 
   const [criptos, setCriptos] = useState([])
   
   useEffect(() => {
-    fetch("https://rest.coincap.io/v3/assets", {
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer 71d696c17b2aa65159d43cd36c081a429fa4e9b1844f2af3b18bc87fa3fe0c54"
-      }})
-      .then((respuesta) => respuesta.json())
-      .then((data) => setCriptos(data.data))
+    axios.get(`${API_URL}assets`, 
+      { headers: { "Authorization": "Bearer 71d696c17b2aa65159d43cd36c081a429fa4e9b1844f2af3b18bc87fa3fe0c54" } })
+      .then((data) => setCriptos(data.data.data))
       .catch(
         (error) => console.log("La petición falló. Error: " + error)
       )
@@ -24,8 +23,8 @@ function App() {
     <>
       <h1>Lista de criptomonedas</h1>
       <ul>
-        {criptos.map(({name, symbol, priceUsd}) => (
-          <li>Nombre: {name} - Símbolo: {symbol} - Precio (USD): {priceUsd}</li>
+        {criptos.map(({id, name, symbol, priceUsd}) => (
+          <li key={id}>Nombre: {name} - Símbolo: {symbol} - Precio (USD): {priceUsd}</li>
         ))}
       </ul>
     </>
