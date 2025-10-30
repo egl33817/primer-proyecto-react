ad4ce8891ee3b252136f25609be4a4bbfde285ce
Roberto Delgado Sanchez <egl33817@educastur.es>
2025-10-30 14:59:01 +0100

    Simplificamos DetalleCripto creando CriptoInfo y CriptoHistorial.

--- src/components/cripto/DetalleCripto.jsx
+++ src/components/cripto/DetalleCripto.jsx
@@ -1,5 +1,7 @@
-import { useParams, Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import usePetition from "../hooks/usePetition.js"
+import CriptoInfo from "./CriptoInfo.jsx"
+import CriptoHistorial from "./CriptoHistorial.jsx"
 import "./DetalleCripto.css"
 
 const DetalleCripto = () => {
@@ -11,72 +13,8 @@ const DetalleCripto = () => {
 
     return (
         <div className="cripto-page-container">
-
-            {
-                datosMoneda && (
-                    <div className="info">
-
-                        <div className="main-info">
-                            <span>Ranking: {datosMoneda.rank}</span>
-                            <h1>{datosMoneda.name}</h1>
-                            <span className="symbol">{datosMoneda.symbol}</span>
-                        </div>
-
-                        <div className="details">
-                            <ul>
-                                <li className="detail">
-                                    <span className="label">Precio (USD): </span>
-                                    <span>{parseFloat(datosMoneda.priceUsd).toFixed(3)}</span>
-                                </li>
-                                <li className="detail">
-                                    <span className="label">MaxSupply: </span>
-                                    <span>{parseFloat(datosMoneda.maxSupply).toFixed(3)}</span>
-                                </li>
-                                <li className="detail">
-                                    <span className="label">Market Cap (USD): </span>
-                                    <span>{parseFloat(datosMoneda.marketCapUsd).toFixed(3)}</span>
-                                </li>
-                                <li className="detail">
-                                    <span className="label">Volumen (USD - 24h): </span>
-                                    <span>{parseFloat(datosMoneda.volumeUsd24Hr).toFixed(3)}</span>
-                                </li>
-                                <li className="detail">
-                                    <span className="label">Variación (24h): </span>
-                                    <span>{parseFloat(datosMoneda.changePercent24Hr).toFixed(3)}</span>
-                                </li>
-                                <li className="detail">
-                                    <span className="label">Vwap (24h): </span>
-                                    <span>{parseFloat(datosMoneda.vwap24Hr).toFixed(3)}</span>
-                                </li>
-                            </ul>
-                        </div> 
-                    </div>
-                )
-            }
-            
-            <div className="history">
-                {
-                    datosHistoricos && (
-                        <table className="history">
-                            <thead>
-                                <tr>
-                                    <th>Fecha</th>
-                                    <th>Precio (USD)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {datosHistoricos.map(({date, priceUsd, time}) => 
-                                    <tr key={time}>
-                                        <td className="label">{new Date(date).toDateString()}</td>
-                                        <td className="price">{parseFloat(priceUsd).toFixed(3)}</td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </table>
-                    )
-                }
-                <Link to="/criptomonedas">Volver...</Link>
-            </div>
+            <CriptoInfo datosMoneda={datosMoneda} />
+            <CriptoHistorial datosHistoricos={datosHistoricos} />
         </div>
     )
 }
