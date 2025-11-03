76c6535a63c2a82cd975a35fb6daaba642e0cc2c
Roberto Delgado Sanchez <egl33817@educastur.es>
2025-11-03 23:00:25 +0100

    Simplificamos el código del componente Cuadricula empleando usePetition.

--- src/components/Cuadricula.jsx
+++ src/components/Cuadricula.jsx
@@ -1,25 +1,12 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import axios from "axios"
 import Cripto from "./cripto/Cripto"
 import "./Cuadricula.css"
+import usePetition from "./hooks/usePetition.js"
 
 function Cuadricula() {
 
-  const API_URL = import.meta.env.VITE_API_URL
+  const criptos = usePetition("")
 
-  const [criptos, setCriptos] = useState([])
-  
-  useEffect(() => {
-    axios.get(`${API_URL}assets`, 
-      { headers: { "Authorization": "Bearer 71d696c17b2aa65159d43cd36c081a429fa4e9b1844f2af3b18bc87fa3fe0c54" } })
-      .then((data) => setCriptos(data.data.data))
-      .catch(
-        (error) => console.log("La petición falló. Error: " + error)
-      )
-  }, [])
-
-  if (criptos.length == 0) return (<h1>Cargando datos...</h1>)
+  if (!criptos) return (<h1>Cargando datos...</h1>)
 
   return (
     <div className="app-container">
