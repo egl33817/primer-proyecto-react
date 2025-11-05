d96599712a591f52c73df23c23bff5f0da63d02b
Roberto Delgado Sanchez <egl33817@educastur.es>
2025-11-05 16:52:29 +0100

    Añadimos la funcionalidad que permite cerrar el login.

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react"
 import { UserContext } from "../../contexts/UserContext"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import "./Menu.css"
 
 const Menu = () => {
     
+    const navegación = useNavigate()
+
     const usuario = useContext(UserContext)
 
     return (
@@ -13,6 +15,10 @@ const Menu = () => {
                 <li><NavLink to="/">Inicio</NavLink></li>
                 <li><NavLink to="/criptomonedas">Lista de criptos</NavLink></li>
                 <li><NavLink to="/perfil">Perfil de {usuario.nombre}</NavLink></li>
+                <li><a onClick={() => {
+                    localStorage.removeItem("tokenCriptoMarket")
+                    navegación("/login")
+                }} >Cerrar sesión</a></li>
             </ul>
         </nav>
     )
